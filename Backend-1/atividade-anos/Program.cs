8789beec2e12663645781c798a1d33f469beb4d4
Mateus Teixeira Paladino <53282675836@info.local>
2023-03-29 16:56:32 -0300

    atividade de C#

---
+++ Backend-1/atividade-anos/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine($"informe seu ano de nascimento");
+int AnoNasci = int.Parse(Console.ReadLine());
+
+
+
+
+
+int Anos = 2023 - AnoNasci ;
+
+int Semanas = AnoNasci * 52;
+
+Console.WriteLine(@$"
+
+idade em Anos {Anos}
+
+idade em semanas {Semanas}
+
+");
+
---
+++ Backend-1/atividade-idade/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine($"Informe sua idade");
+int Idade = int.Parse(Console.ReadLine());
+
+
+int IdadeMeses = Idade * 12;
+int IdadeDias = IdadeMeses * 30;
+int IdadeHoras = IdadeDias * 24;
+int IdadeMinutos = IdadeHoras * 60;
+
+Console.WriteLine(@$"
+    idade em meses {IdadeMeses}
+    idade em dias {IdadeDias}
+    idade em horas {IdadeHoras}
+    idade em minutos {IdadeMinutos}
+");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
