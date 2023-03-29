c97b88884eeb672723e83225dfdbaa2eff5bb6a8
Mateus Teixeira Paladino <53282675836@info.local>
2023-03-29 17:28:35 -0300

    atividade de C#

---
+++ Backend-1/quitanda/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine($"informe o kg");
+float Kg = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"informe o valor do produto");
+float valor = float.Parse(Console.ReadLine());
+
+float compra = 2 * valor;
+
+Console.WriteLine(@$"
+    valor da compra { compra }
+");
+
+
+
