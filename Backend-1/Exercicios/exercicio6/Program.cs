fdcc34fb0eee93efe94c8ee4ed8018c4db7a46da
Mateus Teixeira Paladino <53282675836@info.local>
2023-03-31 08:35:50 -0300

    Lista de exercicio 1

---
+++ Backend-1/Exercicios/exercicio6/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine($"informe a sua frequencia em aulas");
+float frequencia = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Informe sua nota");
+float nota1 = float.Parse(Console.ReadLine());
+
+
+
+if ( frequencia >= 75 && nota1 >= 7.0 )
+
+{
+    Console.WriteLine($"Passou direto");
+}
+
+
+else if ( frequencia >= 75 && nota1 < 7 && nota1 > 3)
+
+{
+    Console.WriteLine($"Voce tem direito a recuperacao");
+    
+}
+
+else 
+
+{
+    Console.WriteLine($"Reprovou");
+    
+}
+
