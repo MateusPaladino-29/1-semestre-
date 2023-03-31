da30233b277524449ca07d0946c9a4235229089a
Mateus Teixeira Paladino <53282675836@info.local>
2023-03-31 10:55:59 -0300

    nova lista de exercicio que falta terminar algumas

---
+++ Backend-1/Lista de exercicios 2/exercicios 4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine($"Telefonou para a vítima?");
+string telefone = Console.ReadLine();
+
+Console.WriteLine($"Esteve no local do crime?");
+string local = Console.ReadLine();
+
+Console.WriteLine($"Mora perto da vitima vítima?");
+string mora = Console.ReadLine();
+
+Console.WriteLine($"Devia para a vítima?");
+string devia = Console.ReadLine();
+
+Console.WriteLine($"Ja trabalhou com a vítima?");
+string trabalhou = Console.ReadLine();
+
+
+if ()
+
+
+
---
+++ back-end paladino/Lista de exercicios 2/exercicio 3/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine($"qual o raio da circuferencia?");
+double raio = double.Parse(Console.ReadLine());
+
+
+ double Diametro = raio * Math.PI;
+ double comprimento =  Diametro *  Math.PI * Math.Pow(1,2);
+ double area =  raio * raio * Math.PI;
+
+ Console.WriteLine(@$"
+    Diametro vai ser igual {Diametro}
+    Comprimento vai ser igual {comprimento}
+    area da circuferencia vai ser igual {area}
+ ");
+ 
+
+
+
