a65559b897ccf84bfcfb92a3d0446f1da13e68dd
Mateus Teixeira Paladino <53282675836@info.local>
2023-04-10 16:25:50 -0300

    lista de exercicio numero 3

--- Backend-1/lista-de-exercicio-3/4/Program.cs
+++ Backend-1/lista-de-exercicio-3/4/Program.cs
@@ -1,54 +1,18 @@
 ﻿
-// int sim = 0;
-
-// int nao = 0;
-
-// //mulheres q responderam sim 
-// int msim = 0;
-
-// int hnao = 0;
-
-
-// string sexo = "";
-// string opn = "";
-
-// for (int i = 1; i <= 10; i++)
-// {
-//     //  Console.WriteLine($"Digite seu sexo:");
-//     //   sexo = Console.ReadLine()!;
-
-//     Console.WriteLine($"Você gostou do produto? responda com sim ou não:");
-//      opn = Console.ReadLine()!;
-
-
-//      if (opn == "sim")
-//      {
-//         sim ++;        
-//      }
-
-//      else
-//      {
-//         nao ++;
-//      }
-
-
-// }
-
-
 
 int pessoasim = 0;
 // int pessoasnao = 0;
 int mulhersim = 0;
 int homemnao = 0;
 
-for (int i = 1; i <= 10; i++)
+for (int i = 1; i <=10 ; i++)
 {
 
     Console.WriteLine($"digite seu genero: M/F");
-    char genero = char.Parse(Console.ReadLine());
+    char genero = char.Parse(Console.ReadLine().ToUpper());
 
     Console.WriteLine($"gostou do produto? S/N");
-    char opn = char.Parse(Console.ReadLine());
+    char opn = char.Parse(Console.ReadLine().ToUpper());
 
     switch (opn)
     {
@@ -79,7 +43,7 @@ Console.WriteLine(@$"
 Número de pessoas que responderam Sim: {pessoasim} pessoas
 Número de pessoas que responderam Não: {10 - pessoasim} pessoas
 Número de mulheres que responderam Sim: {mulhersim} mulheres
-Porcentagem de homens que responderam Não: {homemnao}% homens
+Porcentagem de homens que responderam Não: {homemnao} homens
 ");
 
 
---
+++ Backend-1/lista-de-exercicio-3/5/Program.cs
@@ -0,0 +1,65 @@
+﻿
+int idade = 0;
+float peso = 0;
+char sexo;
+int Totaldemulher = 0;
+int Totaldehomem = 0;
+int idadeHomem = 0;
+double Mediahomem = 0;
+int idadeMulher = 0;
+double Mediamulher = 0;
+int contador = 0;
+
+for (contador = 1; contador <= 3; contador++)
+{
+
+    Console.WriteLine(@$"
+    informe o seu sexo 
+
+    (m) masculino
+    (f) feminino
+    
+    ");
+    sexo = char.Parse(Console.ReadLine()!);
+
+    Console.WriteLine($"informe a sua idade");
+    idade = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine($"informe seu peso");
+    peso = float.Parse(Console.ReadLine()!);
+
+
+    if (sexo == 'm')
+    {
+        Totaldehomem++;
+        idadeHomem = idadeHomem + idade;
+    }
+
+    else if (sexo == 'f')
+    {
+
+        Totaldemulher++;
+        idadeMulher = idadeMulher + idade;
+    }
+
+    else
+    {
+        Console.WriteLine($"esta errado seu idiota");
+        
+    }
+}
+
+Mediahomem = idadeHomem/Totaldehomem;
+
+Mediamulher = idadeMulher/Totaldemulher;
+
+Console.WriteLine(@$"
+
+    A.Total de Homens {Totaldehomem}     
+    B.Total de Mulheres: {Totaldemulher}      
+    C.Média de idade dos Homens: {Math.Round(Mediahomem,2)}  
+    D.Média de idade das mulheres:  {Math.Round(Mediamulher,2)}
+
+");
+
+
