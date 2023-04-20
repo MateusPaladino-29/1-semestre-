9586738a9776578ceb667b36cd3c8fd95fad5a78
Mateus Teixeira Paladino <53282675836@info.local>
2023-04-20 17:05:16 -0300

    POO personagem

---
+++ Backend-1/`POO/aprendendo poo/Personagem.cs
@@ -0,0 +1,30 @@
+namespace aprendendo_poo
+{
+    public class Personagem
+    {
+        public string nome;
+        public int idade;
+        public string armadura;
+        public string ia;
+
+        public void Atacar()
+    {
+        Console.WriteLine($"O personagem Atacou");
+
+    }
+
+    public void Defender()
+    {
+        Console.WriteLine($"O personagem Defendeu");
+
+    }
+    public void RestaurarArmadura()
+    {
+        Console.WriteLine($"O personagem Defendeu");
+
+    }
+    
+    }
+
+}
+
---
+++ Backend-1/`POO/aprendendo poo/Program.cs
@@ -0,0 +1,34 @@
+﻿using aprendendo_poo;
+
+Personagem p1 = new Personagem();
+
+Console.WriteLine($"Informe o nome do novo personagem:");
+p1.nome = Console.ReadLine()!;
+
+
+Console.WriteLine($"Informe a idade do novo personagem:");
+p1.idade = int.Parse(Console.ReadLine()!);
+
+
+Console.WriteLine($"Informe a armadura do novo personagem:");
+p1.armadura = Console.ReadLine()!;
+
+
+Console.WriteLine($"Informe a AI do novo personagem:");
+p1.ia = Console.ReadLine()!;
+
+Console.WriteLine(@$"
+
+{p1.nome}
+{p1.idade}
+{p1.armadura}
+{p1.ia}
+
+");
+
+p1.Atacar();
+p1.Defender();
+p1.RestaurarArmadura();
+
+
+
